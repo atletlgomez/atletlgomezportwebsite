0055bcc3a67b7c574cf9dc02c25dee5fc58eacec
atletlgomez <atletlgomez1@gmail.com>
2024-04-25 01:26:59 -0700

    first push

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div id="nameAndIntro">
+      <h1>ATLETL GOMEZ VARGAS</h1>
+      <h6>
+        I AM CURRENTLY A COLLEGE STUDENT, OCCASIONALLY A PHOTOGRAPHER, BUT
+        ALWAYS A
+      </h6>
+      <h2>web developer.</h2>
+    </div>
+  );
+}
