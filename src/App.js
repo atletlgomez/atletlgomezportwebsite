dc5a1459745ad7ef12aa23753c931911d7bd933b
Atletl Gomez Vargas <atletlgomez1@gmail.com>
2024-05-08 03:58:48 -0700

    google analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Skills from "./components/skills";
 import DisplayProj from "./components/project";
 import Education from "./components/education";
+import { Helmet } from "react-helmet";
 
 import "./styles/projdisplay.css"
 import "./styles/page.css";
@@ -11,18 +12,18 @@ import "./fonts.css";
 function App() {
   return (
     <div className="App">
+        <Helmet>
 
-
-      <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-MP70HJEMFZ"></script>
-  <script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MP70HJEMFZ"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
-  {function gtag(){dataLayer.push(arguments);}}
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-MP70HJEMFZ');
-  </script>
-      </head>
+</script>
+        </Helmet>
+
       <Header></Header>
       <Skills></Skills>
       <DisplayProj></DisplayProj>
